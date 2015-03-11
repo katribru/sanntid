6a5883f103f9b442e6ac9f16e37f9214ed02649a
katribru <katrine.bruvik@gmail.com>
2015-03-11 13:18:51 +0100

    Nesten ferdig nettverksmodul

--- Heis/network.go
+++ Heis/network.go
@@ -7,39 +7,26 @@ import (
 "os" //used in get_local_IPaddr()
 "encoding/json"
 "strings"
-//"strings"
 )
-
-//Solveig IP: 129.241.187.141
-const N_Elevators = 3
-var active_elevators_in_system[N_Elevators]string
-
 
 
 type Message struct {
 		Sender_IP        string
-		Message_type     string //ping, order_table, state_update
-		Data             string //Annen type/container - eks bytes pakken?
+		Message_type     string //ping, order_update elev_state_update
+		Message_content  string //Annen type/container - eks bytes pakken?
 	}
+
+var incoming_message = make(chan Message)
 	
 func spam_ImAlive(){ //Kjøres som en go routine fra main
 	for{
 		broadcast_msg("ping", "ImAlive")
-		time.Sleep(100*time.Millisecond)
+		time.Sleep(300*time.Millisecond)
 	}		
 }
 
-func watch_elevators_in_system(){ //Kjøres når man mottar en ping. Legg denne funksjonalitet til Manager. 
-	//Trenger å vite hvilke heiser som er i systemet
-	var 
-	for{
-		
-	}
-}
-
-
-func broadcast_msg(msg_type string, data string){
-	UDPAdr, err1 := net.ResolveUDPAddr("udp", "129.241.187.255:40101") //Gir hvilken port vi skal broadcaste fra/til? 
+func broadcast_msg(msg_type string, msg_content string){
+	UDPAdr, err1 := net.ResolveUDPAddr("udp", "129.241.187.255:40101") 
 	if err1 != nil{
 		fmt.Println(err1)
 	}
@@ -47,15 +34,13 @@ func broadcast_msg(msg_type string, data string){
 	if err != nil{
 		fmt.Println(err)
 	}
-	
-	
-	//Egen funksjon pack_msg(data)? ---------------
+
 	msg := Message{
 		Sender_IP: get_local_IPadr(),
-		Data: data,
+		Message_type: msg_type,
+		Message_content: msg_content,
 	}
-	//--------------------------------------------
-	//msg := pack_msg(data)	
+	
 	b, err := json.Marshal(msg)
 	if err != nil {
 		fmt.Println("error:", err)
@@ -64,7 +49,7 @@ func broadcast_msg(msg_type string, data string){
 	
 }
 	
-func test_broadcaster(){
+func test_broadcaster(){¨//Tas vekk når vi er ferdig med å teste modulen
 	for i := 0; i < 10; i++ {
 		msg := []string{"Broadcast msg"}
 		broadcast_msg("ping", strings.Join(msg, " "))
@@ -73,7 +58,7 @@ func test_broadcaster(){
 }
 
 
-func receive_msg(){//Kjøres som en go routine fra main? 
+func receive_msg(){//Kjøres som en goroutine fra main 
 	buffer := make([]byte,1024)
 	UDPAdr, err1 := net.ResolveUDPAddr("udp", ":40101")
 	if err1 != nil{
@@ -97,15 +82,13 @@ func receive_msg(){//Kjøres som en go routine fra main?
 		
 		if msg.Sender_IP != get_local_IPadr(){
 			fmt.Println(msg.Sender_IP)
-			//Pass whole struct msg to Manager. Manager wil do different things depending on msg type
-			//Manager.handle_incomming_message(msg)
+			incoming_message <- msg //Sender hele structen
+		
 		}
 		fmt.Println(Addr,n)
 		//fmt.Printf("Rcv message, %d bytes: %s",n,buffer[0:n])
 	}
 }
-
-
 
 
 func get_local_IPadr()string{
@@ -115,28 +98,22 @@ func get_local_IPadr()string{
 		 os.Exit(1)
 	}
 	for _, address := range addrs {
-
-	// check the address type and if it is not a loopback then display it
 		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
 				return ipnet.IP.String()
 			}
 		}
 	}
-	return "Could not get local IP" //Noe bedre å returnere her???
+	return "Error: Could not get local IP" //Noe bedre å returnere her???
 }
 
-
-
-//Ha IPadr i en string. Brukes til identifikasjon. Eks: if !strings.Contains(Addr, ".150"){
-	
 	
 
 func main(){
 	go receive_msg()
 	time.Sleep(3000*time.Millisecond)	
 	go test_broadcaster()
+	go spam_ImAlive()
 	time.Sleep(15000*time.Millisecond)
 	fmt.Println("Done!")
-	
 }
