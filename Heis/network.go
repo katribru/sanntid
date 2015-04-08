9a2e0c7ef25fb15afc534f5bb56b28323b573ffb
katribru <katrine.bruvik@gmail.com>
2015-04-08 14:49:47 +0200

    Noe oppgateringer

--- Heis/network.go
+++ Heis/network.go
@@ -9,13 +9,6 @@ import (
 "strings"
 )
 
-//Elevators_in_system := make(map[string]int) ADD TO MANAGER evt ordremodulen
-
-
-
-
-
-
 /*
 //Flytt til manager!!
 
@@ -65,7 +58,7 @@ type Message struct {
 		Message_content  string //Annen type/container - eks bytes pakken?
 	}
 
-var incoming_message = make(chan Message)
+var Incoming_message = make(chan Message)
 	
 func Spam_ImAlive(){ //Kjøres som en go routine fra main
 	for{
@@ -75,14 +68,9 @@ func Spam_ImAlive(){ //Kjøres som en go routine fra main
 }
 
 func Broadcast_msg(msg_type string, msg_content string){
-	UDPAdr, err1 := net.ResolveUDPAddr("udp", "129.241.187.255:40101") 
-	if err1 != nil{
-		fmt.Println(err1)
-	}
-	UDPconn, err := net.DialUDP("udp", nil, UDPAdr)
-	if err != nil{
-		fmt.Println(err)
-	}
+	UDPAdr, _ := net.ResolveUDPAddr("udp", "129.241.187.255:40101") 
+	
+	UDPconn, _ := net.DialUDP("udp", nil, UDPAdr)
 
 	msg := Message{
 		Sender_IP: get_local_IPadr(),
@@ -90,10 +78,8 @@ func Broadcast_msg(msg_type string, msg_content string){
 		Message_content: msg_content,
 	}
 	
-	b, err := json.Marshal(msg)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
+	b, _ := json.Marshal(msg)
+
 	UDPconn.Write(b)
 	
 }
@@ -106,23 +92,26 @@ func test_broadcaster(){//Tas vekk når vi er ferdig med å teste modulen
 	}
 }
 
+func test_listener(){//Tas vekk når vi er ferdig med å teste modulen
+	for{
+		select{
+		case new_msg := <- Incoming_message:
+			fmt.Println("Case, new_msg: ", new_msg)
+		}
+	}
+}
+
 
 func Receive_msg(){//Kjøres som en goroutine fra main 
 	buffer := make([]byte,1024)
-	UDPAdr, err1 := net.ResolveUDPAddr("udp", ":40101")
-	if err1 != nil{
-		fmt.Println(err1)
-	}
+	UDPAdr, _ := net.ResolveUDPAddr("udp", ":40101")
 	
-	UDPConn, err2 := net.ListenUDP("udp", UDPAdr)
-	if err2 != nil{
-		fmt.Println(err2)
-	}	
+	
+	UDPConn, _ := net.ListenUDP("udp", UDPAdr)
+	
 	for {
-		n, Addr, err3 := UDPConn.ReadFromUDP(buffer)
-		if err3 != nil{
-			fmt.Println(err3)
-		}
+		n, _, _ := UDPConn.ReadFromUDP(buffer)
+		
 		var msg Message
 		err4 := json.Unmarshal(buffer[0:n], &msg)
 		if err4 != nil{
@@ -130,12 +119,11 @@ func Receive_msg(){//Kjøres som en goroutine fra main
 		}
 		
 		if msg.Sender_IP != get_local_IPadr(){
-			fmt.Println(msg.Sender_IP)
-			incoming_message <- msg //Sender hele structen
-			//set_timestamp()
-		
+			fmt.Println("Mottar fra annen IP: ",msg.Sender_IP)
+			Incoming_message <- msg //Sender hele structen
 		}
-		fmt.Println(Addr,n)
+
+		fmt.Println("n: ", n)
 		//fmt.Printf("Rcv message, %d bytes: %s",n,buffer[0:n])
 	}
 }
@@ -160,29 +148,12 @@ func get_local_IPadr()string{
 	
 
 func main(){
-	elevators_in_system := make(map[string]int) //String keys to int values
-	elevators_in_system["123.123"] = 1
-	elevators_in_system["123.124"] = 12
-	elevators_in_system["123.125"] = 3
-	//keys := []string{"123.123","123.124","123.125"}
-	for ip, timestamp := range elevators_in_system {
-	    fmt.Println("Key:", ip, "Value:", timestamp)
-	}
-	elevator_IP := "123.125"
-	elevators_in_system[elevator_IP] = 44
-	for ip, timestamp := range elevators_in_system {
-	    fmt.Println("Key:", ip, "Value:", timestamp)
-	}
-	if _, ok := elevators_in_system[elevator_IP]; !ok { //Dersom key not in map
-		fmt.Println("Key not in map: ", elevator_IP)
-	} 
-
-
 	
 	go Receive_msg()
-	time.Sleep(3000*time.Millisecond)	
-	go test_broadcaster()
+	time.Sleep(3000*time.Millisecond)
+	go test_listener()	
 	go Spam_ImAlive()
+	go test_broadcaster()
 	time.Sleep(15000*time.Millisecond)
 	fmt.Println("Done!")
 }
